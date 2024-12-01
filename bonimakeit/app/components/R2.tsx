7d9f76eda1d312b1238dd000bce2d8a461741eaa
Fabian Boni <58946541+FabianBoni@users.noreply.github.com>
2024-12-01 16:40:32 +0100

    update

--- bonimakeit/app/components/R2.tsx
+++ bonimakeit/app/components/R2.tsx
@@ -6,18 +6,49 @@ import { Howl } from 'howler'
 import { useEffect, useState } from 'react'
 
 function Model() {
+  const [message, setMessage] = useState('')
+  const [currentText, setCurrentText] = useState('')
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const messages = [
+    "Oh hi, you must be a new visitor! Master Fabian is here to greet you!",
+    "Hi, you just met my friend R2D2. He is a bit shy, but he's always happy to meet new people!",
+    "Have fun on my portfolio!"
+  ]
+  const [messageIndex, setMessageIndex] = useState(0)
+
+  useEffect(() => {
+    if (currentIndex < messages[messageIndex].length) {
+      const timer = setTimeout(() => {
+        setMessage(prev => prev + messages[messageIndex][currentIndex])
+        setCurrentIndex(prev => prev + 1)
+      }, 100)
+      return () => clearTimeout(timer)
+    } else {
+      const timer = setTimeout(() => {
+        if (messageIndex < messages.length - 1) {
+          setMessage('')
+          setCurrentIndex(0)
+          setMessageIndex(prev => prev + 1)
+        }
+      }, 2000)
+      return () => clearTimeout(timer)
+    }
+  }, [currentIndex, messageIndex])
+
   const { scene } = useGLTF("/r2d2/scene.gltf", true)
   return (
     <group>
       <primitive object={scene} scale={2.5} />
       <Html
-        position={[2, 4, 0]}
+        position={[4, 2, 0]}  // Moved more to the right
         className="pointer-events-none"
         center
         distanceFactor={15}
       >
-        <div className="hologram-bubble">
-          Hello bonimakeit.ch
+        <div className="hologram-bubble-container">
+          <div className="hologram-bubble">
+            {message}
+          </div>
         </div>
       </Html>
     </group>
@@ -39,7 +70,7 @@ const R2 = () => {
     const playRandomSound = () => {
       const randomIndex = Math.floor(Math.random() * soundFiles.length)
       const sound = new Howl({
-        src: [`/r2d2/sounds/${soundFiles[randomIndex]}`],
+        src: [`/sounds/${soundFiles[randomIndex]}`],
         volume: 0.5,
         loop: false
       })
@@ -60,11 +91,11 @@ const R2 = () => {
       >
         <Environment preset="city" />
         <ambientLight intensity={5} />
-        <spotLight 
-          position={[0, 15, 0]} 
-          angle={0.7} 
-          penumbra={1} 
-          intensity={8} 
+        <spotLight
+          position={[0, 15, 0]}
+          angle={0.7}
+          penumbra={1}
+          intensity={8}
           castShadow
         />
         <directionalLight position={[5, 5, 5]} intensity={3} />
